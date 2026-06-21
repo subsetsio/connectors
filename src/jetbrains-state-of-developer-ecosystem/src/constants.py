bb2d39b79451c96278748f9d47b4c28ac3b526ff
salmiakdrop <nathansnellaert@gmail.com>
2026-06-22 00:06:04 +0200

    deploy itc

---
+++ src/jetbrains-state-of-developer-ecosystem/src/constants.py
@@ -0,0 +1,22 @@
+# Entity union for the JetBrains State of Developer Ecosystem 2025 connector.
+# Each id is a chart slug; the raw CSV lives at
+# https://devecosystem-2025.jetbrains.com/_data/<slug>.csv
+# Copied from data/sources/jetbrains-state-of-developer-ecosystem/work/entity_union.json
+ENTITY_IDS = [
+    "ai_benefits",
+    "ai_coding_delegate",
+    "cloud_dynamics",
+    "concerns_ai_coding",
+    "db7",
+    "dx_org_prod_metrics_grouped",
+    "functionality",
+    "level_ai_adoption_in_company",
+    "net_job_market_sentiment",
+    "pl_dynamics_large",
+    "proglang_plus_adopt",
+    "salary_group_share_by_language",
+    "sw_app_execution",
+    "sw_type",
+    "usage_ai_coding_imputed_final2",
+    "work_where",
+]
