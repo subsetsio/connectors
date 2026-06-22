958e7be843419ec3ffe70476bc30764910ccf2c9
salmiakdrop <nathansnellaert@gmail.com>
2026-06-22 03:04:41 +0200

    deploy maldives-bureau-of-statistics

--- src/maldives-bureau-of-statistics/src/nodes/maldives_bureau_of_statistics.py
+++ src/maldives-bureau-of-statistics/src/nodes/maldives_bureau_of_statistics.py
@@ -49,7 +49,21 @@ _BROWSER_HEADERS = {
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
         "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
     ),
-    "Accept": "*/*",
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;q=0.9,"
+        "image/avif,image/webp,image/apng,*/*;q=0.8"
+    ),
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate",
+    "Referer": INDEX_URL,
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Ch-Ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
 }
 _WAF_CODES = {403, 408, 415, 425, 429}
 
@@ -79,7 +93,7 @@ def _wait(retry_state) -> float:
 
 _waf_retry = tenacity.retry(
     retry=tenacity.retry_if_exception(_retryable),
-    stop=tenacity.stop_after_attempt(10),
+    stop=tenacity.stop_after_attempt(14),
     wait=_wait,
     reraise=True,
 )
