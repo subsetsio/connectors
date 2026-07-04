6166505907ca5feda271a3635aa6e4096b8e7090
salmiakdrop <nathansnellaert@gmail.com>
2026-07-04 15:57:26 +0200

    deploy nasa

--- src/nasa/src/transforms/nasa-events.sql
+++ src/nasa/src/transforms/nasa-events.sql
@@ -8,8 +8,12 @@ SELECT
     "source_id" AS source_id,
     "date" AS observed_at,
     "geom_type" AS geom_type,
-    "longitude" AS longitude,
-    "latitude" AS latitude,
+    -- a few source feeds ship invalid/swapped coordinates (lat up to 200 observed);
+    -- null the pair when either value is out of physical range
+    CASE WHEN "latitude" BETWEEN -90 AND 90 AND "longitude" BETWEEN -180 AND 180
+         THEN "longitude" END AS longitude,
+    CASE WHEN "latitude" BETWEEN -90 AND 90 AND "longitude" BETWEEN -180 AND 180
+         THEN "latitude" END AS latitude,
     "magnitude_value" AS magnitude_value,
     "magnitude_unit" AS magnitude_unit,
     "closed" AS closed_at
