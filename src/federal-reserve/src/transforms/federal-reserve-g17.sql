b8d944e6a8f029cb5fa063006b5fd5b08996528d
salmiakdrop <nathansnellaert@gmail.com>
2026-07-04 12:21:56 +0200

    deploy federal-reserve

---
+++ src/federal-reserve/src/transforms/federal-reserve-g17.sql
@@ -0,0 +1,15 @@
+SELECT
+    series_name                          AS series_id,
+    CAST(time_period AS DATE)            AS date,
+    CAST(obs_value AS DOUBLE)            AS value,
+    frequency,
+    unit,
+    CAST(unit_mult AS DOUBLE)            AS unit_multiplier,
+    NULLIF(currency, 'NA')               AS currency,
+    short_description,
+    long_description,
+    series_attributes
+FROM "federal-reserve-g17"
+WHERE obs_status = 'A'
+  AND obs_value IS NOT NULL
+QUALIFY row_number() OVER (PARTITION BY series_name, time_period) = 1
