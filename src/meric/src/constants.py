b1cbc0685875543568cc8b667e52e83007529dcc
salmiakdrop <nathansnellaert@gmail.com>
2026-07-07 19:01:34 +0200

    deploy opm

---
+++ src/meric/src/constants.py
@@ -0,0 +1,13 @@
+ENTITY_IDS = [
+    "career-pathways",
+    "cost-of-living",
+    "county-average-wages",
+    "current-employment-statistics",
+    "industry-employment-projections",
+    "occupational-employment-wages",
+    "occupational-projections",
+    "qcew-by-geographic-area",
+    "qcew-by-industry",
+    "unemployment-rates",
+    "wioa-county-demographics",
+]
