4f7ea888cf726b6042146850298abdb9c7b5b15f
salmiakdrop <nathansnellaert@gmail.com>
2026-07-10 22:38:09 +0200

    deploy freedom-house

---
+++ src/freedom-house/src/nodes/freedom_house.py
@@ -0,0 +1,18 @@
+from nodes.fiw_all_data import DOWNLOAD_SPECS as FIW_ALL_DATA_SPECS
+from nodes.fiw_all_data import fetch_fiw_all_data
+from nodes.fiw_ratings_statuses import DOWNLOAD_SPECS as FIW_RATINGS_STATUSES_SPECS
+from nodes.fiw_ratings_statuses import fetch_fiw_ratings_statuses
+from nodes.freedom_of_the_press import DOWNLOAD_SPECS as FREEDOM_OF_THE_PRESS_SPECS
+from nodes.freedom_of_the_press import fetch_freedom_of_the_press
+from nodes.freedom_on_the_net import DOWNLOAD_SPECS as FREEDOM_ON_THE_NET_SPECS
+from nodes.freedom_on_the_net import fetch_freedom_on_the_net
+from nodes.nations_in_transit import DOWNLOAD_SPECS as NATIONS_IN_TRANSIT_SPECS
+from nodes.nations_in_transit import fetch_nations_in_transit
+
+DOWNLOAD_SPECS = [
+    *FIW_ALL_DATA_SPECS,
+    *FIW_RATINGS_STATUSES_SPECS,
+    *FREEDOM_OF_THE_PRESS_SPECS,
+    *FREEDOM_ON_THE_NET_SPECS,
+    *NATIONS_IN_TRANSIT_SPECS,
+]
