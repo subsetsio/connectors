a520553b10eb2fbb57e89faafbf549133bb60c80
salmiakdrop <nathansnellaert@gmail.com>
2026-07-10 15:51:06 +0200

    deploy eurocontrol

--- src/epo/src/transforms/epo-top-applicants-by-country.sql
+++ src/epo/src/transforms/epo-top-applicants-by-country.sql
@@ -1,5 +1,7 @@
 SELECT
     country_code,
+    CAST(edition_year AS INTEGER) AS edition_year,
+    CAST(rank AS INTEGER) AS rank,
     applicant_name,
     CAST(number_of_applications AS BIGINT) AS number_of_applications
 FROM "epo-top-applicants-by-country"
--- src/epo/src/transforms/epo-top-applicants-by-field.sql
+++ src/epo/src/transforms/epo-top-applicants-by-field.sql
@@ -1,5 +1,7 @@
 SELECT
     CAST(field_id AS INTEGER) AS field_id,
+    CAST(edition_year AS INTEGER) AS edition_year,
+    CAST(rank AS INTEGER) AS rank,
     applicant_name,
     CAST(number_of_applications AS BIGINT) AS number_of_applications
 FROM "epo-top-applicants-by-field"
