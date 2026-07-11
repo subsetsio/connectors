91f3e8929445cee113760d07f72a25b9b3d15904
salmiakdrop <nathansnellaert@gmail.com>
2026-07-11 04:50:35 +0200

    deploy ireland-property-price-register

--- src/ireland-property-price-register/src/nodes/ireland_property_price_register.py
+++ src/ireland-property-price-register/src/nodes/ireland_property_price_register.py
@@ -32,7 +32,6 @@ from datetime import datetime, timezone
 
 from subsets_utils import (
     NodeSpec,
-    SqlNodeSpec,
     get,
     transient_retry,
     configure_http,
@@ -202,28 +201,5 @@ DOWNLOAD_SPECS = [
         id="ireland-property-price-register-transactions",
         fn=fetch_transactions,
         kind="download",
-    ),
-]
-
-
-TRANSFORM_SPECS = [
-    SqlNodeSpec(
-        id="ireland-property-price-register-transactions-transform",
-        deps=["ireland-property-price-register-transactions"],
-        sql='''
-            SELECT
-                strptime(date_of_sale, '%d/%m/%Y')::DATE                 AS sale_date,
-                file_year                                                AS year,
-                trim(county)                                             AS county,
-                nullif(trim(eircode), '')                                AS eircode,
-                trim(address)                                            AS address,
-                CAST(regexp_replace(price, '[^0-9.]', '', 'g') AS DOUBLE) AS price_eur,
-                (not_full_market_price = 'Yes')                          AS not_full_market_price,
-                (vat_exclusive = 'Yes')                                  AS vat_exclusive,
-                description_of_property                                  AS description,
-                nullif(trim(property_size_description), '')              AS property_size_description
-            FROM "ireland-property-price-register-transactions"
-            WHERE date_of_sale IS NOT NULL AND date_of_sale <> ''
-        ''',
     ),
 ]
