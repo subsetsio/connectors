eb112287f1e0d5019b24f65f904c2e151af0a844
salmiakdrop <nathansnellaert@gmail.com>
2026-07-11 21:00:03 +0200

    deploy parlgov

--- src/parlgov/src/transforms/parlgov-data-party-family.sql
+++ src/parlgov/src/transforms/parlgov-data-party-family.sql
@@ -1,7 +1,6 @@
 SELECT
     CAST(id AS INTEGER)       AS id,
-    description,
+    CAST(description AS VARCHAR) AS description,
     CAST(type_id AS INTEGER)  AS type_id,
     CAST(party_id AS INTEGER) AS party_id
 FROM "parlgov-data-party-family"
-
