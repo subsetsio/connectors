275e71b26fe462f701d5e43c21699640ebdf42fe
salmiakdrop <nathansnellaert@gmail.com>
2026-07-11 07:21:52 +0200

    deploy ipea

--- src/ipea/src/transforms/ipea-values.sql
+++ src/ipea/src/transforms/ipea-values.sql
@@ -2,8 +2,8 @@ SELECT
     SERCODIGO                              AS series_code,
     CAST(substr(VALDATA, 1, 10) AS DATE)   AS date,
     CAST(VALVALOR AS DOUBLE)               AS value,
-    NULLIF(NIVNOME, '')                    AS geo_level,
-    NULLIF(TERCODIGO, '')                  AS territory_code
+    COALESCE(NULLIF(NIVNOME, ''), 'not_applicable') AS geo_level,
+    COALESCE(NULLIF(TERCODIGO, ''), 'not_applicable') AS territory_code
 FROM "ipea-values"
 WHERE VALVALOR IS NOT NULL
   AND VALDATA IS NOT NULL
