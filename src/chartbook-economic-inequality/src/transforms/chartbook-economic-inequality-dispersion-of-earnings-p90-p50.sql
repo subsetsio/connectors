5b285abd186834821d461c55fbf6f9e79af995b3
salmiakdrop <nathansnellaert@gmail.com>
2026-07-12 17:11:47 +0200

    deploy chartbook-economic-inequality

---
+++ src/chartbook-economic-inequality/src/transforms/chartbook-economic-inequality-dispersion-of-earnings-p90-p50.sql
@@ -0,0 +1,11 @@
+SELECT
+    CAST(country     AS VARCHAR) AS country,
+    CAST(year        AS INTEGER) AS year,
+    CAST(dimension   AS VARCHAR) AS dimension_of_inequality,
+    CAST(measure     AS VARCHAR) AS measure_of_inequality,
+    CAST(series_key  AS VARCHAR) AS series_key,
+    CAST(series      AS INTEGER) AS series,
+    CAST(description AS VARCHAR) AS description,
+    CAST(value       AS DOUBLE)  AS value
+FROM "chartbook-economic-inequality-dispersion-of-earnings-p90-p50"
+WHERE value IS NOT NULL
