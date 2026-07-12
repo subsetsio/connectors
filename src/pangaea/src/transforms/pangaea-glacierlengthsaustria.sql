6e9964772a29ebae1bfa46167c36c85e2a8d55a9
salmiakdrop <nathansnellaert@gmail.com>
2026-07-13 00:20:56 +0200

    deploy pangaea

--- src/pangaea/src/transforms/pangaea-glacierlengthsaustria.sql
+++ src/pangaea/src/transforms/pangaea-glacierlengthsaustria.sql
@@ -1,21 +1,23 @@
 SELECT
     "collection"::VARCHAR AS collection,
     "source_doi"::VARCHAR AS source_doi,
-    "dataset_doi"::VARCHAR AS dataset_doi,
-    "file_name"::VARCHAR AS file_name,
-    "source_row_number"::BIGINT AS source_row_number,
-    TRY_CAST(
-        regexp_extract(
-            max(CASE WHEN "column_name" = 'Date/Time' THEN "value" END)
-                OVER (PARTITION BY "dataset_doi", "file_name", "source_row_number"),
-            '([0-9]{4})',
-            1
+    COALESCE("dataset_doi", "source_doi")::VARCHAR AS dataset_doi,
+    COALESCE("file_name", '__source_manifest__')::VARCHAR AS file_name,
+    COALESCE("source_row_number", 0)::BIGINT AS source_row_number,
+    CASE
+        WHEN "record_type" = 'cell' THEN TRY_CAST(
+            regexp_extract(
+                max(CASE WHEN "column_name" = 'Date/Time' THEN "value" END)
+                    OVER (PARTITION BY "dataset_doi", "file_name", "source_row_number"),
+                '([0-9]{4})',
+                1
+            )
+            AS BIGINT
         )
-        AS BIGINT
-    ) AS observation_year,
-    "column_position"::BIGINT AS column_position,
-    "column_name"::VARCHAR AS source_column_name,
-    "value"::VARCHAR AS value_text,
+        ELSE NULL::BIGINT
+    END AS observation_year,
+    COALESCE("column_position", 0)::BIGINT AS column_position,
+    COALESCE("column_name", 'source_doi_manifest')::VARCHAR AS source_column_name,
+    COALESCE("value", "source_doi")::VARCHAR AS value_text,
     "metadata_text"::VARCHAR AS metadata_text
 FROM "pangaea-glacierlengthsaustria"
-WHERE "record_type" = 'cell'
