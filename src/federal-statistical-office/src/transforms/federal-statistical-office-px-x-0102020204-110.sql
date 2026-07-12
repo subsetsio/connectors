b921470415a6ab0f2eee66d39c9bf3740fe236a4
salmiakdrop <nathansnellaert@gmail.com>
2026-07-12 14:01:33 +0200

    deploy federal-statistical-office

--- src/federal-statistical-office/src/transforms/federal-statistical-office-px-x-0102020204-110.sql
+++
@@ -1,9 +0,0 @@
--- provisional pass-through for PxWeb cube; regenerate from the settled model after model-verify.
-SELECT
-    CAST("year" AS VARCHAR) AS "year",
-    CAST("demographic_characteristic" AS VARCHAR) AS "demographic_characteristic",
-    CAST("sex" AS VARCHAR) AS "sex",
-    CAST(value AS DOUBLE) AS value,
-    CAST(cube_id AS VARCHAR) AS cube_id,
-    CAST(updated AS VARCHAR) AS updated
-FROM "federal-statistical-office-px-x-0102020204-110"
