8c6e1aa3b507eaf6c0139932ab4fbe92de636d13
salmiakdrop <nathansnellaert@gmail.com>
2026-07-13 01:21:35 +0200

    deploy silso

---
+++ src/silso/src/transforms/silso-daily-estimated.sql
@@ -0,0 +1,8 @@
+SELECT
+    make_date(year, month, day)              AS date,
+    NULLIF(estimated_sunspot_number, -1)     AS estimated_sunspot_number,
+    NULLIF(estimated_std_dev, -1)            AS estimated_std_dev,
+    NULLIF(n_calculated, -1)                 AS n_calculated,
+    NULLIF(n_available, -1)                  AS n_available
+FROM "silso-daily-estimated"
+WHERE NULLIF(estimated_sunspot_number, -1) IS NOT NULL
