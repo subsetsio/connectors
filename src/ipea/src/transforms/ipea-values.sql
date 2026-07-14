f27c4af5bb1b17c82c08b9d92c77868867d48427
salmiakdrop <nathansnellaert@gmail.com>
2026-07-14 12:41:08 +0200

    deploy ipea

--- src/ipea/src/transforms/ipea-values.sql
+++ src/ipea/src/transforms/ipea-values.sql
@@ -7,3 +7,6 @@ SELECT DISTINCT
 FROM "ipea-values"
 WHERE VALVALOR IS NOT NULL
   AND VALDATA IS NOT NULL
+  -- IPEA uses 9999-01-01 for undated climate-normal series; exclude them from
+  -- this temporal observations table so date freshness remains meaningful.
+  AND substr(VALDATA, 1, 4) <> '9999'
