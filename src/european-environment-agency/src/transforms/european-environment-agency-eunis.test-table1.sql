f2dec16a15b8ae7ac34e64bb5a11882d4f4cc326
salmiakdrop <nathansnellaert@gmail.com>
2026-07-16 15:07:51 +0200

    deploy european-environment-agency

--- src/european-environment-agency/src/transforms/european-environment-agency-eunis.test-table1.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    CAST("data" AS VARCHAR) AS "data",
-    CAST("description" AS VARCHAR) AS "description",
-    CAST("id" AS VARCHAR) AS "id",
-    CAST("name" AS VARCHAR) AS "name",
-    CAST("valid" AS VARCHAR) AS "valid"
-FROM "european-environment-agency-eunis.test-table1"
