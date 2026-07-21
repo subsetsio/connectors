ef356ec4cf3620482fe065f8eab633338b92568e
salmiakdrop <nathansnellaert@gmail.com>
2026-07-21 16:56:47 +0200

    deploy statistics-estonia

---
+++ src/statistics-estonia/src/transforms/statistics-estonia-td077.px.sql
@@ -0,0 +1,9 @@
+-- provisional pass-through for newly accepted PxWeb table; regenerate after model-verify.
+SELECT
+    "indicator",
+    "occupation_category",
+    "institutional_sector",
+    "level_of_education",
+    "reference_period",
+    "value"
+FROM "statistics-estonia-td077.px"
---
+++ src/statistics-estonia/src/transforms/statistics-estonia-to65.px.sql
@@ -0,0 +1,7 @@
+-- provisional pass-through for newly accepted PxWeb table; regenerate after model-verify.
+SELECT
+    "indicator",
+    "product_ttl",
+    "reference_period",
+    "value"
+FROM "statistics-estonia-to65.px"
