17be02b52817ce9a662a86501f157ea1c185a53a
salmiakdrop <nathansnellaert@gmail.com>
2026-07-26 00:18:15 +0200

    deploy ipea

--- src/ipea/src/transforms/ipea-values.sql
+++ src/ipea/src/transforms/ipea-values.sql
@@ -1,6 +1,10 @@
 SELECT
     SERCODIGO                              AS series_code,
-    CAST(substr(VALDATA, 1, 10) AS DATE)   AS date,
+    CASE
+        WHEN substr(VALDATA, 1, 10) = '9999-01-01' THEN NULL
+        ELSE CAST(substr(VALDATA, 1, 10) AS DATE)
+    END                                    AS date,
+    substr(VALDATA, 1, 10) = '9999-01-01' AS has_unknown_date,
     CAST(VALVALOR AS DOUBLE)               AS value,
     COALESCE(NULLIF(NIVNOME, ''), 'not_applicable') AS geo_level,
     COALESCE(NULLIF(TERCODIGO, ''), 'not_applicable') AS territory_code
