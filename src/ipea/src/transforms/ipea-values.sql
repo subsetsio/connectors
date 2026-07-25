a673863bd725273d3d9a7d78e9c8ead39d562c54
salmiakdrop <nathansnellaert@gmail.com>
2026-07-25 23:12:43 +0200

    deploy ipea

--- src/ipea/src/transforms/ipea-values.sql
+++ src/ipea/src/transforms/ipea-values.sql
@@ -1,6 +1,9 @@
 SELECT DISTINCT
     SERCODIGO                              AS series_code,
-    CAST(substr(VALDATA, 1, 10) AS DATE)   AS date,
+    CASE
+        WHEN substr(VALDATA, 1, 4) = '9999' THEN NULL
+        ELSE CAST(substr(VALDATA, 1, 10) AS DATE)
+    END                                    AS date,
     CAST(VALVALOR AS DOUBLE)               AS value,
     COALESCE(NULLIF(NIVNOME, ''), 'not_applicable') AS geo_level,
     COALESCE(NULLIF(TERCODIGO, ''), 'not_applicable') AS territory_code
@@ -9,4 +12,4 @@ WHERE VALVALOR IS NOT NULL
   AND VALDATA IS NOT NULL
   -- IPEA uses 9999-01-01 for undated climate-normal series; exclude them from
   -- this temporal observations table so date freshness remains meaningful.
-  AND substr(VALDATA, 1, 4) <> '9999'
+  AND date IS NOT NULL
