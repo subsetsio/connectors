dec2491ebab4d58215ebd8595bb5b46810d9ab5c
salmiakdrop <nathansnellaert@gmail.com>
2026-07-26 07:58:36 +0200

    deploy statswales

--- src/statswales/src/transforms/statswales-d11ca4b4-2cc5-4695-924b-54152498e316.sql
+++ src/statswales/src/transforms/statswales-d11ca4b4-2cc5-4695-924b-54152498e316.sql
@@ -7,6 +7,6 @@ SELECT
     raw."Data description" AS data_description,
     raw."Population" AS population,
     raw."Local Authority" AS local_authority,
-    strptime(raw."Year", '%d/%m/%Y')::DATE AS year,
+    strptime(raw."Year ending", '%d/%m/%Y')::DATE AS year,
     raw."Notes" AS notes
 FROM "statswales-d11ca4b4-2cc5-4695-924b-54152498e316" AS raw
